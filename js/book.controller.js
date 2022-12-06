47e73c1f5d66b759d5006cb1b5a3eb7484f346de
Neta Dror <neta.l.dror@gmail.com>
2022-12-06 14:20:04 +0200

    search bug fix

---
+++ js/book.controller.js
@@ -0,0 +1,125 @@
+'use strict'
+
+function onInit() {
+    renderFilterByQueryStringParams()
+    renderBooks()
+}
+function renderBooks() {
+    var books = getBooks()
+    var strHtmls = books.map(book => `
+    <tr>
+            <td>${book.id}</td>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>${book.rate}</td>
+            <td><button class="btnRead" onclick="onReadBook('${book.id}')">read</button></td>
+            <td><button class="btnUpdate" onclick="onUpdateBook('${book.id}')">update</button></td>
+            <td><button class="btnDelete" onclick="onRemoveBook('${book.id}')">delete</button></td>
+     </tr> `
+    )
+    document.querySelector('.books-container').innerHTML = strHtmls.join('')
+}
+function onReadBook(bookId) {
+    var book = getBookById(bookId)
+    var elModal = document.querySelector('.modal')
+    elModal.querySelector('.bookId').innerText = book.id
+    elModal.querySelector('.bookTitle').innerText = book.title
+    elModal.querySelector('.bookPrice').innerText = book.price
+    elModal.querySelector('.rate').innerText = book.rate
+    elModal.classList.add('open')
+}
+function onRemoveBook(bookId) {
+    removeBook(bookId)
+}
+function onUpdateBook(bookId) {
+    var bookPrice = prompt('new book price?')
+    updateBook(bookId, bookPrice)
+}
+function onCloseModal() {
+    document.querySelector('.modal').classList.remove('open')
+}
+function onPlusRate() {
+    var elModal = document.querySelector('.modal')
+    var ElbookId = elModal.querySelector('.bookId').innerText
+    updateBookRatingPlus(ElbookId)
+    updateModalRating(ElbookId)
+    renderBooks()
+}
+function onMinusRate() {
+    var elModal = document.querySelector('.modal')
+    var ElbookId = elModal.querySelector('.bookId').innerText
+    updateBookRatingMinus(ElbookId)
+    updateModalRating(ElbookId)
+    renderBooks()
+}
+function updateModalRating(ElbookId) {
+    var book = getBookById(ElbookId)
+    var elModal = document.querySelector('.modal')
+    elModal.querySelector('.rate').innerText = book.rate
+}
+function onSetFilterBy(key, value) {
+    // console.log('key, value', key, value)
+    var filterBy = setBookFilter(key, value)
+    console.log('filterBy', filterBy)
+    // renderBooks()
+    // const queryStringParams = `?maxPrice=${gFilterBy.maxPrice}&minRate=${gFilterBy.minRate}&search=${gFilterBy.search}`
+    // const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + queryStringParams
+    // window.history.pushState({ path: newUrl }, '', newUrl)
+}
+function onSearch() {
+    var userSearch = document.querySelector('.search').value
+    console.log(userSearch)
+    onSetFilterBy('search', userSearch)
+
+}
+function onNextPage() {
+    var elPrevBtn = document.querySelector('.btnBack')
+    elPrevBtn.classList.contains('disabled') ? elPrevBtn.classList.remove('disabled') : ''
+    nextPage()
+    renderBooks()
+}
+function onPrevPage() {
+    var elNextBtn = document.querySelector('.btnNext')
+    elNextBtn.classList.contains('disabled') ? elNextBtn.classList.remove('disabled') : ''
+    prevPage()
+    renderBooks()
+}
+function renderFilterByQueryStringParams() {
+    const queryStringParams = new URLSearchParams(window.location.search)
+    const filterBy = {
+        maxPrice: queryStringParams.get('maxPrice') || '',
+        minRate: +queryStringParams.get('minRate') || 0,
+        search: +queryStringParams.get('search') || 0
+
+    }
+
+    if (!filterBy.maxPrice && !filterBy.minRate && !filterBy.search) return
+
+    document.querySelector('.filter-price').value = filterBy.maxPrice
+    document.querySelector('.filter-rate').value = filterBy.minRate
+    document.querySelector('.search').value = filterBy.search
+    setBookFilter(filterBy)
+}
+function toggleDisableNext() {
+    var elNextBtn = document.querySelector('.btnNext')
+
+    if (elNextBtn.disabled) {
+        elNextBtn.disabled = false
+        console.log('elNextBtn.disabled', elNextBtn.disabled)
+    } else {
+        elNextBtn.disabled = true
+        console.log('elNextBtn.disabled', elNextBtn.disabled)
+    }
+
+}
+function toggleDisablePrev() {
+    var elPrevBtn = document.querySelector('.btnBack')
+
+    if (elPrevBtn.disabled) {
+        elPrevBtn.disabled = false
+        console.log('elPrevBtn.disabled', elPrevBtn.disabled)
+    } else {
+        elPrevBtn.disabled = true
+        console.log('elPrevBtn.disabled', elPrevBtn.disabled)
+    }
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,150 @@
+'use strict'
+const STORAGE_KEY = 'bookDB'
+const PAGE_SIZE = 5
+
+
+var gPageIdx = 0
+var gBooks
+var gFilterBy = { maxPrice: 200, minRate: 0, search: '' }
+
+_createBooks()
+
+function _createBooks() {
+    var books = loadFromStorage(STORAGE_KEY)
+    console.log('books', books)
+    // Nothing in storage - generate demo data
+    if (!books || !books.length) {
+        books = [
+            _createBook('book one', 18.90, 2),
+            _createBook('book two', 60.65, 5),
+            _createBook('book three', 75.20, 8),
+            _createBook('book four', 96, 2),
+            _createBook('book five', 26, 1),
+            _createBook('book six', 74, 7),
+            _createBook('book seven', 62.20, 9),
+            _createBook('book eight', 46, 3),
+            _createBook('book nine', 76, 0),
+            _createBook('book ten', 13, 10)
+        ]
+    }
+
+    gBooks = books
+    console.log('gBooks', gBooks)
+    _saveBooksToStorage()
+}
+function _createBook(title, price, rate) {
+    return {
+        id: makeId(),
+        title: title,
+        price: price,
+        rate: rate
+    }
+}
+function _saveBooksToStorage() {
+    saveToStorage(STORAGE_KEY, gBooks)
+}
+function removeBook(bookId) {
+    console.log('bookId', bookId)
+    var removedBookId = gBooks.findIndex(book => bookId === book.id)
+    gBooks.splice(removedBookId, 1)
+    console.log('gBooks', gBooks)
+    _saveBooksToStorage()
+    renderBooks()
+}
+function onAddBook() {
+    var addedBookName = prompt('book name?')
+    var addedBookPrice = prompt('book price?')
+    console.log('addedBookName, addedBookPrice', addedBookName, addedBookPrice)
+    addBook(addedBookName, addedBookPrice)
+}
+function addBook(addedBookName, addedBookPrice) {
+    var newBook = _createBook(addedBookName, addedBookPrice)
+    gBooks.push(newBook)
+    _saveBooksToStorage()
+    renderBooks()
+}
+function updateBook(bookId, bookPrice) {
+    var updatedBook = gBooks.find(book => book.id === bookId)
+    updatedBook.price = bookPrice
+    _saveBooksToStorage()
+    renderBooks()
+}
+function updateBookRatingPlus(bookId) {
+    // console.log('bookId', bookId)
+    var updatedBook = gBooks.find(book => book.id === bookId)
+    // console.log('updatedBook', updatedBook)
+    updatedBook.rate = updatedBook.rate + 1
+    console.log('updatedBook', updatedBook)
+    _saveBooksToStorage()
+    renderBooks()
+}
+function updateBookRatingMinus(bookId) {
+    // console.log('bookId', bookId)
+    var updatedBook = gBooks.find(book => bookId === book.id)
+    console.log('updatedBook', updatedBook)
+    updatedBook.rate = updatedBook.rate - 1
+    console.log('updatedBook', updatedBook)
+    _saveBooksToStorage()
+    renderBooks()
+}
+function getBookById(bookId) {
+    // console.log('bookId', bookId)
+    const book = gBooks.find(book => bookId === book.id)
+    // console.log('book', book)
+    return book
+}
+function setBookFilter(key, value) {
+    console.log('key, value', key, value)
+    gPageIdx = 0
+    if (value !== undefined) gFilterBy[key] = value
+    console.log('gFilterBy', gFilterBy)
+    return gFilterBy
+}
+function getBooks() {
+    var searchKey = gFilterBy.search
+    console.log('searchKey', searchKey)
+
+    if (searchKey) {
+        var books = gBooks.filter(book => book.title.includes(searchKey))
+    }
+    else {
+        books = gBooks.filter(book => {
+            return book.price <= gFilterBy.maxPrice &&
+                book.rate >= gFilterBy.minRate
+        })
+    }
+    console.log('books', books)
+    var startIdx = gPageIdx * PAGE_SIZE
+    return books.slice(startIdx, startIdx + PAGE_SIZE)
+}
+function nextPage() {
+    if (gPageIdx * PAGE_SIZE >= gBooks.length - 1) {
+        // gPageIdx = gPageIdx * PAGE_SIZE
+        gPageIdx = 0
+        console.log('gPageIdx too big', gPageIdx)
+        // toggleDisableNext()
+    } else {
+        if (gPageIdx === 0) {
+            // toggleDisablePrev()
+        }
+        gPageIdx++
+        console.log('gPageIdx', gPageIdx)
+
+    }
+}
+function prevPage() {
+    if (gPageIdx * PAGE_SIZE <= 0) {
+        gPageIdx = 0
+        toggleDisablePrev()
+        console.log('gPageIdx too small', gPageIdx)
+    }
+    else {
+        if (gPageIdx * PAGE_SIZE >= gBooks.length - 1) {
+            toggleDisableNext()
+        }
+        gPageIdx--
+        console.log('gPageIdx', gPageIdx)
+    }
+}
+
+
